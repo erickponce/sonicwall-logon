961be87c5b4056e9d061bc6a293976957dd7ce82
Erick Ponce <erickponceleao@gmail.com>
2017-12-18 16:53:17 -0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,14 @@ PROCESS_NAME="sonicwall-logon"
 PROCESS_BASE_PATH="/opt/"$PROCESS_NAME"/"
 PROCESS_LOG="/var/log/"$PROCESS_NAME".log"
 CONFIG_FILE_PATH="/etc/"$PROCESS_NAME"/auth.conf"
+
+PYTHON_ENV="$HOME/venv-sonicwall"
+if [ ! -d "$PYTHON_ENV" ]; then
+    if [ ! -f "/usr/bin/virtualenv" ]; then
+        sudo apt-get install -yq virtualenv
+    fi
+    virtualenv -p /usr/bin/python3 $PYTHON_ENV
+fi
+source $PYTHON_ENV/bin/activate
+
 python -u $PROCESS_BASE_PATH"auto_logon.py" -c $CONFIG_FILE_PATH
