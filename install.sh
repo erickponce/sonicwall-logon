1dac6ebfa332942a31ebb11de5e72375dbeabb85
Erick Almeida <ephillipe@gmail.com>
2018-05-25 14:42:34 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ rm -Rf /tmp/sonicwall-logon
 git clone https://github.com/erickponce/sonicwall-logon.git  /tmp/sonicwall-logon
 cd /tmp/sonicwall-logon
 sudo chmod a+x setup.sh
-sudo ./setup.sh
+sudo bash setup.sh
