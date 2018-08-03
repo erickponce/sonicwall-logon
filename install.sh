d54b4b24a1cfb7cd26c4da7d05c2ccd51ada2a56
Erick Almeida <ephillipe@gmail.com>
2018-08-03 14:22:12 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-sudo apt install cowsay -y
-sudo ln -s /usr/games/cowsay /bin/cowsay
-
-cowsay -f tux "\n[Installing SonicWall Auto Logon]\n"
+echo "\n[Installing SonicWall Auto Logon]\n"
 
 rm -Rf /tmp/sonicwall-logon
 git clone https://github.com/erickponce/sonicwall-logon.git  /tmp/sonicwall-logon
