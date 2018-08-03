cd1f7f8b904c9a8a77ba4c0b2226545e42d3878d
Erick Almeida <ephillipe@gmail.com>
2018-08-03 14:22:43 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "\n[Installing SonicWall Auto Logon]\n"
 
+HERE=$(pwd)
 rm -Rf /tmp/sonicwall-logon
 git clone https://github.com/erickponce/sonicwall-logon.git  /tmp/sonicwall-logon
 cd /tmp/sonicwall-logon
 sudo chmod a+x setup.sh
 sudo bash setup.sh
+cd $HERE
 rm -Rf /tmp/sonicwall-logon
